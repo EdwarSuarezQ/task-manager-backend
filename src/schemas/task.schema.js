c30c63ea128f039fbea3db5d181e02b47f427306
edwarSuarezQ <145814931+edwarSuarezQ@users.noreply.github.com>
2025-09-18 13:48:41 -0500

    feature: Agregando nuevo campo completado.

--- src/schemas/task.schema.js
+++ src/schemas/task.schema.js
@@ -1,11 +1,12 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const createTaskSchame = z.object({
-    title: z.string( {
-        required_error: "title is required."
-    } ),
-    description: z.string( {
-        required_error: "description muts be a string."
-    } ),
-    date: z.string( {} ).datetime().optional(),
+  title: z.string({
+    required_error: "title is required.",
+  }),
+  description: z.string({
+    required_error: "description muts be a string.",
+  }),
+  date: z.string({}).datetime().optional(),
+  completed: z.boolean().optional(),
 });
